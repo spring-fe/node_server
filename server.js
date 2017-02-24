796e33be0934605c18e8bcf0d222c01fe5d3ff3c
spring-fe <1906982292@qq.com>
2017-02-23 19:39:39 -0800

    node_server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var PORT = 3000;
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-var mine = require('./mine').types;
+var mime = require('./mime').types;
 var path = require('path');
 
 var server = http.createServer(function(request, response){
@@ -33,7 +33,7 @@ var server = http.createServer(function(request, response){
 					});
 					response.end(err);
 				}else{
-					var contentType = mine[ext] || "text/plain";
+					var contentType = mime[ext] || "text/plain";
 					console.log(contentType)
 					response.writeHead(200, {
 						'Content-Type' : contentType
